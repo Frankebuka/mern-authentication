7b2e3508419f0535a5360b025e34ad316b56a912
Frankebuka <frankebuka@yahoo.com>
2023-10-21 10:15:58 +0000

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from "../redux/user/userSlice";
-// import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 // import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  // const { loading, error } = useSelector((state) => state.user);
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -21,7 +21,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -31,13 +31,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        // dispatch(signInFailure(data));
+        dispatch(signInFailure(data));
         return;
       }
-      // dispatch(signInSuccess(data));
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      // dispatch(signInFailure(error));
+      dispatch(signInFailure(error));
     }
   };
   return (
@@ -59,10 +59,10 @@ export default function SignIn() {
           onChange={handleChange}
         />
         <button
-          // disabled={loading}
+          disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {/* {loading ? "Loading..." : "Sign In"} */}
+          {loading ? "Loading..." : "Sign In"}
         </button>
         {/* <OAuth /> */}
       </form>
@@ -73,7 +73,7 @@ export default function SignIn() {
         </Link>
       </div>
       <p className="text-red-700 mt-5">
-        {/* {error ? error.message || "Something went wrong!" : ""} */}
+        {error ? error.message || "Something went wrong!" : ""}
       </p>
     </div>
   );
